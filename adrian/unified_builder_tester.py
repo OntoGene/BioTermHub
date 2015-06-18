626c963761d91d367600cb17e126ff17d52751bf
vicawil <avdl@gmx.ch>
2015-06-18 16:42:13 +0200

    intermediary

--- adrian/unified_builder_tester.py
+++ adrian/unified_builder_tester.py
@@ -1,37 +1,54 @@
 from unified_builder import RecordSetContainer, UnifiedBuilder
 
-test_rsc = RecordSetContainer("1k_snippets/BIOGRID-IDENTIFIERS-1k",
-                              #"1k_snippets/BIOGRID-ALL-1k",
-                              "1k_snippets/uniprot_sprot-1k",
-                              #"1k_snippets/gene_info_10k")
+test_rsc = RecordSetContainer("data/BIOGRID-IDENTIFIERS-3.4.125.tab.txt",
+                              None #"1k_snippets/BIOGRID-IDENTIFIERS-1k",
+                              #"data/uniprot_sprot.dat",
+                              None #"data/gene_info"
+                              )
+
+#~ test_rsc = RecordSetContainer("1k_snippets/BIOGRID-IDENTIFIERS-1k",
+                              #~ #"1k_snippets/BIOGRID-ALL-1k"
+                              #~ "1k_snippets/uniprot_sprot-1k",
+                              #~ None #"1k_snippets/gene_info_10k"
+                              #~ )
 
 biogrid_idents = test_rsc.biogrid_idents
 #biogrid_records = test_rsc.biogrid_records
 uniprot_records = test_rsc.uniprot_records
 #entrezgene_records = test_rsc.entrezgene_records
 
-
-counter_uni = 0
 counter_ent = 0
+counter_uni = 0
 
-missing_UNIPROT = []
 missing_ENTREZ_GENE = []
+missing_UNIPROT = []
 
 for ident in biogrid_idents:
+    if "ENTREZ_GENE" in biogrid_idents[ident]:
+        counter_ent += 1
+        if not biogrid_idents[ident]["ENTREZ_GENE"] in entrezgene_records:
+            missing_ENTREZ_GENE.append(biogrid_idents[ident]["ENTREZ_GENE"])
+
     if "UNIPROT-ACCESSION" in biogrid_idents[ident]:
         counter_uni += 1
         if not biogrid_idents[ident]["UNIPROT-ACCESSION"] in uniprot_records:
             missing_UNIPROT.append(biogrid_idents[ident]["UNIPROT-ACCESSION"])
 
-#~ if "ENTREZ_GENE" in biogrid_idents[ident]:
-        #~ counter_ent += 1
-        #~ if not biogrid_idents[ident]["ENTREZ_GENE"] in entrezgene_records:
-            #~ missing_ENTREZ_GENE.append(biogrid_idents[ident]["ENTREZ_GENE"])
-            
-print len(missing_UNIPROT)
-print counter_uni
 print len(missing_ENTREZ_GENE)
 print counter_ent
+print len(missing_UNIPROT)
+print counter_uni
+
+import codecs
+
+missing_e = codecs.open("missing_e", "w", "utf-8")
+missing_u = codecs.open("missing_u", "w", "utf-8")
+
+missing_e.writelines(missing_ENTREZ_GENE)
+missing_u.writelines(missing_UNIPROT)
+
+missing_e.close()
+missing_u.close()
 
 #~ print "\nBIOGRID Records"
 #~ print biogrid_records[0:5]
