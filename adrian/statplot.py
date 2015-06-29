c799a724d1d1e5018c532a74f2d8e90e07edbbb8
vicawil <avdl@gmx.ch>
2015-06-29 14:38:27 +0200

    -

--- adrian/statplot.py
+++ adrian/statplot.py
@@ -16,60 +16,60 @@ def plotstats(rsc):
     stats = rsc.stats
 
     file_counter = 0
-for resource in stats:
-    overall = stats[resource].copy()
-    ratios_raw = overall.pop("ratios")
-    if resource != "total":
-        ratios = OrderedDict(sorted(ratios_raw.items(), key = lambda x: x[0][0]))
-    else:
-        ratios = ratios_raw
-
-    for label, statdict in [("overall", overall), ("ratios", ratios)]:
-        if resource == "total" and label == "ratios":
-            ratio_dicts = [OrderedDict(sorted(statdict["terms/id"].items(), key = lambda x: x[0][0])),
-                           OrderedDict(sorted(statdict["ids/term"].items(), key = lambda x: x[0][0]))]
+    for resource in stats:
+        overall = stats[resource].copy()
+        ratios_raw = overall.pop("ratios")
+        if resource != "total":
+            ratios = OrderedDict(sorted(ratios_raw.items(), key = lambda x: x[0][0]))
         else:
-            ratio_dicts = [statdict]
-        for statdict in ratio_dicts:
-            X = np.arange(len(statdict))
-            setlog = True if resource == "total" and label == "overall" else False
-            try:
-                statdict_keys = [count for count, label in statdict]
-                fig = plt.bar(statdict_keys, statdict.values(), align='center', width=0.15, log=setlog)
-            except:
-                fig = plt.bar(X, statdict.values(), align='center', width=0.15, log=setlog)
+            ratios = ratios_raw
 
-            try:
-                statdict_keys = [count for count, label in statdict]
-            except ValueError:
-                statdict_keys = statdict.keys()
+        for label, statdict in [("overall", overall), ("ratios", ratios)]:
+            if resource == "total" and label == "ratios":
+                ratio_dicts = [OrderedDict(sorted(statdict["terms/id"].items(), key = lambda x: x[0][0])),
+                               OrderedDict(sorted(statdict["ids/term"].items(), key = lambda x: x[0][0]))]
+            else:
+                ratio_dicts = [statdict]
+            for statdict in ratio_dicts:
+                X = np.arange(len(statdict))
+                setlog = True if resource == "total" and label == "overall" else False
+                try:
+                    statdict_keys = [count for count, label in statdict]
+                    fig = plt.bar(statdict_keys, statdict.values(), align='center', width=0.15, log=setlog)
+                except:
+                    fig = plt.bar(X, statdict.values(), align='center', width=0.15, log=setlog)
 
-            if label == "overall":
-                plt.xticks(X, statdict_keys)
-            elif label == "ratios":
-                plt.gca().xaxis.set_major_locator(MaxNLocator(15))
+                try:
+                    statdict_keys = [count for count, label in statdict]
+                except ValueError:
+                    statdict_keys = statdict.keys()
 
-            ymax_val = max(statdict.values())
-            ymax = math.ceil(ymax_val + ymax_val / 100) if ymax_val > 20 else ymax_val + 2
-            xmax= len(statdict)
+                if label == "overall":
+                    plt.xticks(X, statdict_keys)
+                elif label == "ratios":
+                    plt.gca().xaxis.set_major_locator(MaxNLocator(15))
 
-            ymin = 0 if ymax_val < 300 else math.ceil(ymax_val/100)
-            plt.ylim(0, ymax)
-            plt.grid(True)
-            plt.xlabel(label)
-            plt.ylabel('count')
-            # plt.xlim(xmax - 0.5, xmax + 0.5)
-            # plt.figtext(0.5, 0.85, resource + " - %s" % label, color='black', weight='roman', size='x-large')
-            # plt.autoscale(tight=False)
+                ymax_val = max(statdict.values())
+                ymax = math.ceil(ymax_val + ymax_val / 100) if ymax_val > 20 else ymax_val + 2
+                xmax= len(statdict)
 
-            # try:
-            #     plt.show()
-            # except TypeError:
-            #     print "No display available."
+                ymin = 0 if ymax_val < 300 else math.ceil(ymax_val/100)
+                plt.ylim(0, ymax)
+                plt.grid(True)
+                plt.xlabel(label)
+                plt.ylabel('count')
+                # plt.xlim(xmax - 0.5, xmax + 0.5)
+                # plt.figtext(0.5, 0.85, resource + " - %s" % label, color='black', weight='roman', size='x-large')
+                # plt.autoscale(tight=False)
 
-            plt.savefig(STATSPATH + str(file_counter) + "_" + resource + "_" + label.replace("/", "_per_"), bbox_inches='tight', dpi=200)
-            plt.clf()
-            file_counter += 1
+                # try:
+                #     plt.show()
+                # except TypeError:
+                #     print "No display available."
+
+                plt.savefig(STATSPATH + str(file_counter) + "_" + resource + "_" + label.replace("/", "_per_"), bbox_inches='tight', dpi=200)
+                plt.clf()
+                file_counter += 1
 
     plt.close()
 
