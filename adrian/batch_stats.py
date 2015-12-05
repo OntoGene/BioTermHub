61483e55a20bfb39488de44f8b255e55da752a66
Lenz Furrer <furrer@cl.uzh.ch>
2015-12-05 12:33:27 +0100

    bugfix: don't attempt to delete a file for each line

--- adrian/batch_stats.py
+++ adrian/batch_stats.py
@@ -39,7 +39,8 @@ def main():
         print "Sending message ..."
 
         send_mail(cs.address, [mail], 'Statistics for %s' % export_name, 'Hello,\nYou have requested statistics plots for %s. Find them attached.' % export_name, cs, files)
-        remove(PENDING)
+
+    remove(PENDING)
 
 
 if __name__ == "__main__":
