7d87c75f916e7e77cc1dffd17c12a58b33e1766c
Lenz <Lenz.Furrer@gmail.com>
2016-06-24 17:34:13 +0200

    intermediate abstract OBO parser filter class

---
+++ inputfilters/_obo.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+# coding: utf8
+
+# Author: Lenz Furrer, 2016
+
+
+'''
+Parser for OBO files.
+'''
+
+
+import re
+
+from termhub.inputfilters._base import AbstractRecordSet
+from termhub.lib.tools import Fields
+
+
+class OboRecordSet(AbstractRecordSet):
+    '''
+    Abstract record collector for OBO dumps.
+    '''
+    def __iter__(self):
+        '''
+        Iterate over term entries (1 per synonym).
+        '''
+        for concept in self._iter_stanzas():
+            oid = next(self.oidgen)
+
+            terms = concept['synonyms']
+            terms.add(concept['pref'])
+
+            if self.collect_stats:
+                self.update_stats(len(terms))
+
+            for term in terms:
+                entry = Fields(oid,
+                               self.resource,
+                               concept['id'],
+                               term,
+                               concept['pref'],
+                               self.entity_type)
+                yield entry
+
+    def _iter_stanzas(self):
+        '''
+        Parse the .obo stanzas.
+
+        Do not call list(...) on this method:
+        The same object is yielded in every iteration
+        (with modified content).
+        '''
+        tag_value = re.compile(r'(\w+): (.+)')
+        synonym_type = re.compile(r'"((?:[^"]|\\")*)" (.+)')
+
+        with open(self.fn, encoding='utf-8') as f:
+            inside = False
+            concept = {}
+            for line in f:
+                line = line.strip()
+                if not line:
+                    # Stanza has ended.
+                    if 'id' in concept:
+                        yield concept
+                    inside = False
+                    concept.clear()
+                elif line == '[Term]':
+                    # Stanza starts.
+                    inside = True
+                    concept['synonyms'] = set()
+                elif line == 'is_obsolete: true':
+                    continue
+                elif inside:
+                    tag, value = tag_value.match(line).groups()
+                    if tag == 'id':
+                        concept['id'] = value
+                    elif tag == 'namespace':
+                        concept['entity_type'] = value
+                    elif tag == 'name':
+                        concept['pref'] = value
+                    elif tag == 'synonym':
+                        synonym, syntype = synonym_type.match(value).groups()
+                        if self._relevant_synonym(syntype):
+                            # Unescape quotes.
+                            synonym = synonym.replace('\\"', '"')
+                            concept['synonyms'].add(synonym)
+            if 'id' in concept:
+                # After the final stanza: last yield.
+                yield concept
+
+    @classmethod
+    def _relevant_synonym(cls, syntype):
+        '''
+        Select synonym type.
+        '''
+        return True
--- inputfilters/chebi.py
+++ inputfilters/chebi.py
@@ -11,11 +11,10 @@ Collect ChEBI chemicals ("chebi.obo").
 
 import re
 
-from termhub.inputfilters._base import AbstractRecordSet
-from termhub.lib.tools import Fields
+from termhub.inputfilters._obo import OboRecordSet
 
 
-class RecordSet(AbstractRecordSet):
+class RecordSet(OboRecordSet):
     '''
     Record collector for ChEBI.
     '''
@@ -24,68 +23,6 @@ class RecordSet(AbstractRecordSet):
     resource = 'ChEBI'
     entity_type = 'chemical'
     dump_fn = 'chebi.obo'
-
-    def __iter__(self):
-        '''
-        Iterate over term entries (1 per synonym).
-        '''
-        for concept in self._iter_stanzas():
-            oid = next(self.oidgen)
-
-            terms = set(s for s, t in concept['synonyms']
-                          if self._relevant_synonym(t))
-            terms.add(concept['pref'])
-
-            if self.collect_stats:
-                self.update_stats(len(terms))
-
-            for term in terms:
-                entry = Fields(oid,
-                               self.resource,
-                               concept['id'],
-                               term,
-                               concept['pref'],
-                               self.entity_type)
-                yield entry
-
-    def _iter_stanzas(self):
-        '''
-        Parse the .obo stanzas.
-
-        Do not call list(...) on this method:
-        The same object is yielded in every iteration
-        (with modified content).
-        '''
-        tag_value = re.compile(r'(\w+): (.+)')
-        synonym_type = re.compile(r'"(.*)" (.+)')
-
-        with open(self.fn, encoding='utf-8') as f:
-            inside = False
-            concept = {}
-            for line in f:
-                line = line.strip()
-                if not line:
-                    # Stanza has ended.
-                    if 'id' in concept:
-                        yield concept
-                    inside = False
-                    concept.clear()
-                elif line == '[Term]':
-                    # Stanza starts.
-                    inside = True
-                    concept['synonyms'] = []
-                elif inside:
-                    tag, value = tag_value.match(line).groups()
-                    if tag == 'id':
-                        concept['id'] = value
-                    elif tag == 'name':
-                        concept['pref'] = value
-                    elif tag == 'synonym':
-                        synonym, syntype = synonym_type.match(value).groups()
-                        concept['synonyms'].append((synonym, syntype))
-            if 'id' in concept:
-                # After the final stanza: last yield.
-                yield concept
 
     @classmethod
     def _relevant_synonym(cls, syntype):
