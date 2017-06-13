4edf8d36ca3a6f6c7c378e5cd60d69d41a95631d
Lenz <Lenz.Furrer@gmail.com>
2017-06-13 11:02:25 +0200

    uniprot: move xml parsing to preprocessing

--- inputfilters/uniprot.py
+++ inputfilters/uniprot.py
@@ -23,7 +23,7 @@ class RecordSet(AbstractRecordSet):
     resource = 'Swiss-Prot'
     entity_type = 'gene/protein'
 
-    dump_fn = 'uniprot_sprot.xml'
+    dump_fn = 'uniprot_sprot.tsv'
     remote = ('ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/'
               'knowledgebase/complete/uniprot_sprot.xml.gz')
     source_ref = 'http://web.expasy.org/docs/swiss-prot_guideline.html'
@@ -49,24 +49,35 @@ class RecordSet(AbstractRecordSet):
 
     def _iter_concepts(self):
         '''
+        Get the pre-extracted information from TSV.
+        '''
+        with open(self.fn, encoding='utf-8') as f:
+            for line in f:
+                id_, pref, *terms = line.rstrip('\n').split('\t')
+                yield id_, pref, terms
+
+    @classmethod
+    def preprocess(cls, stream):
+        '''
         Extract the relevant information.
         '''
         # Precompose the Xpaths including namespace.
-        entry_tag = self._ns('entry')
-        id_tag = self._ns('accession')
-        pref_tag = self._ns('protein', 'recommendedName', 'fullName')
-        syn_tags = [self._ns('protein', status, length)
+        entry_tag = cls._ns('entry')
+        id_tag = cls._ns('accession')
+        pref_tag = cls._ns('protein', 'recommendedName', 'fullName')
+        syn_tags = [cls._ns('protein', status, length)
                     for status in ('recommendedName', 'alternativeName')
                     for length in ('fullName', 'shortName')]
 
-        for _, entry in etree.iterparse(self.fn, tag=entry_tag):
-            id_ = entry.find(id_tag).text
-            pref = entry.find(pref_tag).text
-            synonyms = set(s.text
+        for _, entry in etree.iterparse(stream, tag=entry_tag):
+            id_ = cls._untab(entry.find(id_tag).text)
+            pref = cls._untab(entry.find(pref_tag).text)
+            synonyms = set(cls._untab(s.text)
                            for t in syn_tags
                            for s in entry.iterfind(t))
             entry.clear()
-            yield id_, pref, synonyms
+            line = '{}\t{}\t{}\n'.format(id_, pref, '\t'.join(synonyms))
+            yield line.encode('utf-8')
 
     @staticmethod
     def _ns(*tags):
@@ -76,6 +87,13 @@ class RecordSet(AbstractRecordSet):
         ns = '{http://uniprot.org/uniprot}'
         return '/'.join('{}{}'.format(ns, tag) for tag in tags)
 
+    @staticmethod
+    def _untab(text):
+        '''
+        Replace tabs and newlines with spaces.
+        '''
+        return text.replace('\t', ' ').replace('\n', ' ').replace('\r', ' ')
+
     @classmethod
     def update_info(cls):
-        return cls._update_info(['gz'])
+        return cls._update_info(['gz', cls.preprocess])
