bc7b5b18a7634e49bb73d05aa0fea50d8965cd4b
Nicola Colic <ncolic@gmail.com>
2018-08-22 00:09:37 +0200

    added prefered_term method

--- inputfilters/rxnorm.py
+++ inputfilters/rxnorm.py
@@ -47,14 +47,71 @@ class RecordSet(IterConceptRecordSet):
     
     @classmethod
     def preprocess(self, stream):
-        '''
-        Save some space by removing unused data right away.
-        '''
+        
+        # general file format as described here:
+        # https://www.nlm.nih.gov/research/umls/rxnorm/docs/2018/rxnorm_doco_full_2018-1.html#s12_4
+        # row[0]: ID, row[11]: source, row[12]: term type, row[14]: term string
         reader = csv.reader(stream,delimiter='|')
+        
+        # we keep the last read line in memory, 
+        # as well as list
+        # of all term variations for the same ID
+        last_row = next(reader)
+        current_id = last_row[0]
+        terms = [ last_row[14] ]
+        
         for row in reader:
-            print(row)
-            line = '\t'.join([row[0],row[14],str((row[14],)),self.entity_type,self.resource]) + '\n'
-            yield line.encode('utf-8')
+            
+            # if ID is the same, then we just
+            # add terms to the list
+            if current_id == row[0]:
+                terms.append(row[14])
+            
+            # otherwise we write line
+            # and update / reset counters etc.   
+            else:     
+                terms_string = '(' + ', '.join(terms) + ')'
+                line = '\t'.join([last_row[0],self.prefered_term(terms),terms_string,last_row[12],last_row[11]]) + '\n'
+                
+                current_id = row[0]
+                last_row = row
+                terms = [ row[14] ]
+                
+                yield line.encode('utf-8')
+        
+        # write the last line
+        terms_string = '(' + ', '.join(terms) + ')'
+        line = '\t'.join([last_row[0],self.prefered_term(terms),terms_string,last_row[12],last_row[11]]) + '\n'
+        yield line.encode('utf-8')
+
+    @staticmethod        
+    def prefered_term(terms):
+        '''
+        Given list of term variations, finds what is most likely to be the preferred term
+        * groups all variations by their lowercased version, then takes the most frequent group
+        * if there's a match, takes the longest string
+        '''        
+        
+        lower = [ term.lower() for term in terms ]
+        
+        # if they're all the same, there's nothing for us to do
+        if len(set(lower)) == 1:
+            return lower[0]
+        
+        # let's see if one get's the most counts
+        counts = { term : lower.count(term) for term in set(lower) }
+        max_count = max(counts.values())
+        frequentest_terms = [ term for term, count in counts.items() if count == max_count ]
+        if len(frequentest_terms) == 1:
+            return frequentest_terms[0]
+        
+        # take the longest one from those that have the most counts
+        lengths = { term : len(term) for term in frequentest_terms }
+        max_length = max(lengths.values())
+        longest_terms = [ term for term, length in lengths.items() if length == max_length ]
+        
+        # if they have equal length, also, then we just take the first one
+        return longest_terms[0] 
             
     def _iter_body(self):
         '''
