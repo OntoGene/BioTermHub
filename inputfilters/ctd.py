8d30dc2c6e1dd9b9c89cb018edc3d387edeb7967
Lenz <Lenz.Furrer@gmail.com>
2018-12-21 15:17:42 +0100

    perform CTD parsing during update, not aggregating

--- inputfilters/ctd.py
+++ inputfilters/ctd.py
@@ -6,10 +6,11 @@
 
 '''
 Collect CTD chemicals and diseases
-("CTD_chemicals.csv" and "CTD_diseases.csv").
+("CTD_chemicals.tsv" and "CTD_diseases.tsv").
 '''
 
 
+import io
 import csv
 import itertools as it
 
@@ -42,37 +43,44 @@ class RecordSet(IterConceptRecordSet):
         self._exclude = frozenset(exclude)
 
     def _iter_concepts(self):
+        for id_, ns, pref, *terms in self._concept_rows():
+            terms = set(t for t in terms if (id_, t) not in self._exclude)
+            if terms:
+                resource = self._resource_mapping[ns]
+                yield id_, pref, terms, self.entity_type, resource
+
+    _line_template = '{id}\t{ns}\t{pref}\t{terms}\n'
+
+    @classmethod
+    def preprocess(cls, stream):
         '''
         Parse CSV and extract the relevant information.
         '''
         # The CTD format seems to use the default CSV properties
         # (delimiter comma and minimal quoting with '"').
-        reader = csv.reader(self._iter_body())
+        reader = csv.reader(cls._iter_body(stream))
         for name, id_, _, _, _, _, _, synonyms, _ in reader:
             ns, id_ = id_.split(':')  # split away the namespace prefix
-            synonyms = synonyms.split('|') if synonyms else []
-            synonyms.append(name)
-            terms = set(term for term in synonyms
-                        if (id_, term) not in self._exclude)
-            if terms:
-                resource = self._resource_mapping[ns]
-                yield id_, name, terms, self.entity_type, resource
+            terms = synonyms.split('|') if synonyms else []
+            terms.append(name)
+            yield cls._canonical_line(id=id_, ns=ns, pref=name, terms=terms)
 
-    def _iter_body(self):
+    @staticmethod
+    def _iter_body(stream):
         '''
         Iterate over the lines following the header lines.
         '''
-        with open(self.fn, encoding='utf-8', newline='') as f:
-            # Skip initial lines until one without leading "#" is found.
-            yield from it.dropwhile(lambda line: line.startswith('#'), f)
+        stream = io.TextIOWrapper(stream, encoding='utf-8', newline='')
+        # Skip initial lines until one without leading "#" is found.
+        return it.dropwhile(lambda line: line.startswith('#'), stream)
 
     @classmethod
     def resource_names(cls):
         return list(cls._resource_names.values())
 
-    @staticmethod
-    def _update_steps():
-        return ('gz',)
+    @classmethod
+    def _update_steps(cls):
+        return ('gz', cls.preprocess)
 
 
 class ChemRecordSet(RecordSet):
@@ -82,7 +90,7 @@ class ChemRecordSet(RecordSet):
 
     entity_type = 'chemical'
 
-    dump_fn = 'CTD_chemicals.csv'
+    dump_fn = 'CTD_chemicals.tsv'
     remote = 'http://ctdbase.org/reports/CTD_chemicals.csv.gz'
 
     @classmethod
@@ -97,7 +105,7 @@ class DiseaseRecordSet(RecordSet):
 
     entity_type = 'disease'
 
-    dump_fn = 'CTD_diseases.csv'
+    dump_fn = 'CTD_diseases.tsv'
     remote = 'http://ctdbase.org/reports/CTD_diseases.csv.gz'
 
     @classmethod
